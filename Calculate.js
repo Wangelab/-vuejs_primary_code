e79e62636438f1f0f7ae94552e6422d54c8c7a84
Wangelab <37280230+Wangelab@users.noreply.github.com>
2018-04-26 10:09:00 +0800

    计算属性
    计算属性js

---
+++ Calculate.js
@@ -0,0 +1,19 @@
+var cal = new Vue({
+	el:"#calcul",
+	data:{
+		math:40,
+		physics:70,
+		english:102
+	},
+	// 计算属性
+	computed:{
+		sum:function(){
+			return parseFloat(this.math) + parseFloat(this.physics) + parseFloat(this.english);
+		},
+		average:function(){
+			aver = Math.round(this.sum / 3.0);
+			return aver;
+		}
+	}
+
+});
