2876f36ce5c02efd2d22ef4e638e2147841523ff
Wangelab <37280230+Wangelab@users.noreply.github.com>
2018-05-10 15:20:34 +0800

    用于绑定 html 属性

---
+++ v-bind.js
@@ -0,0 +1,11 @@
+var bind = new Vue({
+	el:'#bind',
+	data:{
+		
+		url:"http://www.baidu.com",
+		ktn:"btn btn-defult",
+		isActive:true ,
+		img:"https://www.jianshu.com/apps/download?utm_source=sbc"
+
+	}
+});
